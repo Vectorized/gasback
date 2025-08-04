f94e37fac0bc391e435ecf848810a4ede4ea39b4
Vectorized <webby1111@hotmail.com>
2025-08-04 09:19:56 +0000

    Fix script

--- script/Delegate7702.s.sol
+++ script/Delegate7702.s.sol
@@ -8,35 +8,25 @@ contract Delegate7702Script is Script {
     function run() external {
         uint256 privateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
 
-        address gasbackImplementation = _deployWithNicks(type(Gasback).creationCode);
+        address nicks = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
+        address gasbackImplementation =
+            _predictDeterministicAddress(keccak256(type(Gasback).creationCode), 0, nicks);
+        if (gasbackImplementation.code.length == 0) {
+            vm.startBroadcast(privateKey);
+            gasbackImplementation = address(new Gasback{salt: 0}());
+            vm.stopBroadcast();
+        }
+
         address deployer = vm.addr(privateKey);
 
         vm.signAndAttachDelegation(gasbackImplementation, privateKey);
-        bytes memory code = deployer.code;
-        require(code.length > 0, "Not delegation detected");
 
         vm.startBroadcast(privateKey);
+        Gasback(payable(deployer)).noop();
         Gasback(payable(deployer)).setGasbackRatioNumerator(900000000000000000);
-        vm.stopBroadcast();
-
-        vm.startBroadcast(privateKey);
         Gasback(payable(deployer)).setGasbackMaxBaseFee(type(uint256).max);
-        vm.stopBroadcast();
-
-        vm.startBroadcast(privateKey);
         Gasback(payable(deployer)).setBaseFeeVault(0x4200000000000000000000000000000000000019);
         vm.stopBroadcast();
-    }
-
-    /// @dev Returns the address deployed via Nick's factory with salt `bytes32(0)`.
-    /// If the contract has already been deployed, skips deploying it.
-    function _deployWithNicks(bytes memory code) internal returns (address) {
-        address nicks = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
-        address instance = _predictDeterministicAddress(keccak256(code), 0, nicks);
-        if (instance.code.length != 0) return instance;
-        (bool success,) = nicks.call(abi.encodePacked(bytes32(0), code));
-        require(success && instance.code.length != 0, "Deployment via Nick's failed.");
-        return instance;
     }
 
     /// @dev Returns the address when a contract with initialization code hash,
